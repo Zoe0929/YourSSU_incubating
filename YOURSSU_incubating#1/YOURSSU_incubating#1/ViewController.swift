11a47e8f007a2e525ba042f7293c624332a7e603
Zoe <68178395+Zoe0929@users.noreply.github.com>
2022-04-14 18:13:58 +0900

    AutoLayout 설정

--- YOURSSU_incubating#1/YOURSSU_incubating#1/ViewController.swift
+++ YOURSSU_incubating#1/YOURSSU_incubating#1/ViewController.swift
@@ -17,57 +17,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func touchUpPlusButton(_ sender: UIButton)
+    @IBAction func touchUpOpButton(_ sender: UIButton)
     {
-        guard let num1 = Num1.text, let num2 = Num2.text else {
-            return
-        }
-        var result = 0
-        if let inum1 = Int(num1), let inum2=Int(num2) {
-            result = inum1 + inum2
-        }
-        resultText.text="\(num1)+\(num2)=\(result)"
-    }
-
-    
-    @IBAction func touchUpMinusButton(_ sender: UIButton)
-    {
-            guard let num1 = Num1.text, let num2 = Num2.text else {
-                return
+        if let num1 = Num1.text, let num2 = Num2.text {
+            
+            if num1.isEmpty || num2.isEmpty {
+                resultText.text = "값을 모두 입력해주세요."
             }
-            var result = 0
-            if let inum1 = Int(num1), let inum2=Int(num2) {
-                result = inum1 - inum2
-            }
-            resultText.text="\(num1)-\(num2)=\(result)"
-    }
-    
-    @IBAction func touchUpMutiButton(_ sender: UIButton)
-    {
-                guard let num1 = Num1.text, let num2 = Num2.text else {
+            else if let inum1 = Int(num1), let inum2 = Int(num2){
+//                resultText.text="\(buttonId.tag)"
+                switch sender.tag{
+                case 0:
+                    resultText.text="\(num1)+\(num2)=\(inum1+inum2)"
+                case 1:
+                    resultText.text="\(num1)-\(num2)=\(inum1-inum2)"
+                case 2:
+                    if(inum1==0 || inum2==0){
+                        resultText.text="0으로는 나눌 수 없습니다."
+                    }
+                    else{
+                        resultText.text="\(num1)/\(num2)=\(inum1/inum2)"
+                    }
+                case 3:
+                    resultText.text="\(num1)*\(num2)=\(inum1*inum2)"
+                default:
                     return
                 }
-                var result = 0
-                if let inum1 = Int(num1), let inum2=Int(num2) {
-                    result = inum1 * inum2
-                }
-                resultText.text="\(num1)*\(num2)=\(result)"
+            }
+            else {
+                resultText.text="잘못된 값입니다."
+            }
+        }
     }
-    
-    @IBAction func touchUpDivButton(_ sender: UIButton)
-    {
-                guard let num1 = Num1.text, let num2 = Num2.text else {
-                    return
-                }
-                var result = 0
-                if let inum1 = Int(num1), let inum2=Int(num2) {
-                    result = inum1 / inum2
-                }
-                resultText.text="\(num1)/\(num2)=\(result)"
-    }
-    
-    
-    
-    
 }
 
