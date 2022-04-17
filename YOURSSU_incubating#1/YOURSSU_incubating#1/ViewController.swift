1a65e1c1ba1570d38ff4cbd91e5f85f5b3de82cd
Zoe <68178395+Zoe0929@users.noreply.github.com>
2022-04-18 04:15:53 +0900

    codeReview 2

--- YOURSSU_incubating#1/YOURSSU_incubating#1/ViewController.swift
+++ YOURSSU_incubating#1/YOURSSU_incubating#1/ViewController.swift
@@ -8,9 +8,10 @@
 import UIKit
 class ViewController: UIViewController {
     
-    @IBOutlet weak var num1: UITextField!
-    @IBOutlet weak var num2: UITextField!
+    @IBOutlet weak var num1TextField: UITextField!
+    @IBOutlet weak var num2TextField: UITextField!
     @IBOutlet weak var resultText: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,27 +20,27 @@ class ViewController: UIViewController {
 
     @IBAction func touchUpOpButton(_ sender: UIButton)
     {
-        if let num1 = Num1.text, let num2 = Num2.text {
+        if let num1 =  num1TextField.text, let num2 =  num2TextField.text {
             
             if num1.isEmpty || num2.isEmpty {
                 resultText.text = "값을 모두 입력해주세요."
             }
-            else if let inum1 = Int(num1), let inum2 = Int(num2){
+            else if let num1int = Int(num1), let num2int = Int(num2){
 //                resultText.text="\(buttonId.tag)"
                 switch sender.tag{
                 case 0:
-                    resultText.text="\(num1)+\(num2)=\(inum1+inum2)"
+                    resultText.text="\(num1)+\(num2)=\(num1int+num2int)"
                 case 1:
-                    resultText.text="\(num1)-\(num2)=\(inum1-inum2)"
+                    resultText.text="\(num1)-\(num2)=\(num1int-num2int)"
                 case 2:
-                    if(inum2==0){
+                    if(num2int==0){
                         resultText.text="0으로는 나눌 수 없습니다."
                     }
                     else{
-                        resultText.text="\(num1)/\(num2)=\(inum1/inum2)"
+                        resultText.text="\(num1)/\(num2)=\(num1int/num2int)"
                     }
                 case 3:
-                    resultText.text="\(num1)*\(num2)=\(inum1*inum2)"
+                    resultText.text="\(num1)*\(num2)=\(num1int*num2int)"
                 default:
                     return
                 }
