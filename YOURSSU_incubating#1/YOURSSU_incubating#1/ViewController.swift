feeb8e1274c81285b8e3aec59c0ec9625851ae6a
Zoe <68178395+Zoe0929@users.noreply.github.com>
2022-05-03 18:23:40 +0900

    코드리뷰 3: 변수명 변경

--- YOURSSU_incubating#1/YOURSSU_incubating#1/ViewController.swift
+++ YOURSSU_incubating#1/YOURSSU_incubating#1/ViewController.swift
@@ -10,7 +10,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var num1TextField: UITextField!
     @IBOutlet weak var num2TextField: UITextField!
-    @IBOutlet weak var resultText: UILabel!
+    @IBOutlet weak var resultTextUIlabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -23,30 +23,30 @@ class ViewController: UIViewController {
         if let num1 =  num1TextField.text, let num2 =  num2TextField.text {
             
             if num1.isEmpty || num2.isEmpty {
-                resultText.text = "값을 모두 입력해주세요."
+                resultTextUIlabel.text = "값을 모두 입력해주세요."
             }
             else if let num1int = Int(num1), let num2int = Int(num2){
-//                resultText.text="\(buttonId.tag)"
+//                resultTextUIlabel.text="\(buttonId.tag)"
                 switch sender.tag{
                 case 0:
-                    resultText.text="\(num1)+\(num2)=\(num1int+num2int)"
+                    resultTextUIlabel.text="\(num1)+\(num2)=\(num1int+num2int)"
                 case 1:
-                    resultText.text="\(num1)-\(num2)=\(num1int-num2int)"
+                    resultTextUIlabel.text="\(num1)-\(num2)=\(num1int-num2int)"
                 case 2:
                     if(num2int==0){
-                        resultText.text="0으로는 나눌 수 없습니다."
+                        resultTextUIlabel.text="0으로는 나눌 수 없습니다."
                     }
                     else{
-                        resultText.text="\(num1)/\(num2)=\(num1int/num2int)"
+                        resultTextUIlabel.text="\(num1)/\(num2)=\(num1int/num2int)"
                     }
                 case 3:
-                    resultText.text="\(num1)*\(num2)=\(num1int*num2int)"
+                    resultTextUIlabel.text="\(num1)*\(num2)=\(num1int*num2int)"
                 default:
                     return
                 }
             }
             else {
-                resultText.text="잘못된 값입니다."
+                resultTextUIlabel.text="잘못된 값입니다."
             }
         }
     }
