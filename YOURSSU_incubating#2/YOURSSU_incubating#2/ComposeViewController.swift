32334c96df31f6d1f152109c76cb0ab8bb180960
Zoe <68178395+Zoe0929@users.noreply.github.com>
2022-05-14 17:49:11 +0900

    branch

---
+++ YOURSSU_incubating#2/YOURSSU_incubating#2/ComposeViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ComposeViewController.swift
+//  YOURSSU_incubating#2
+//
+//  Created by 지희의 MAC on 2022/05/09.
+//
+
+import UIKit
+ 
+
+
+
+
+
+class ComposeViewController: UIViewController {
+
+ 
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var memoTextView: UITextView!
+    let textViewPlaceHolder = "내용 입력"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        memoTextView.delegate=self
+        //
+        titleTextField.placeholder="제목 입력"
+        memoTextView.text=textViewPlaceHolder
+        memoTextView.textColor = .lightGray
+        
+        
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //save 버튼 동작
+    @IBAction func save(_ sender: Any) {
+        let title = titleTextField.text
+        let memo = memoTextView.text
+        let newMemo = Memo(title: title ?? "기본제목" , content: memo ?? "기본내용"   )
+        MemoList.shared.dummyMemoList.append(newMemo)
+        //델리게이트, 노티피케이션 센터 이용해서 데이터 넘겨주기
+        NotificationCenter.default.post(name: ComposeViewController.newMemoDisInsert, object: nil)
+        //화면 전환 방법이 present -> push 로 바뀌었으니 뒤로 가는 방법도 바뀜!(pop 이용)
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension ComposeViewController{
+    static let newMemoDisInsert = Notification.Name(rawValue: "newMemoDidInsert")
+}
+
+extension ComposeViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == textViewPlaceHolder {
+            textView.text = nil
+            textView.textColor = .black
+        }
+        
+    }
+
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            textView.text = textViewPlaceHolder
+            textView.textColor = .lightGray
+        }
+    }
+
+}
